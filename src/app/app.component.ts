1a674ea636e5bde9d829bdf597e350c369cd2520
Lorena Ponsico <lorena.poca@gmail.com>
2023-10-17 16:53:16 +0200

    service API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RickAndMortyService } from './_services/rick-and-morty.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'rick-and-morty';
+export class AppComponent implements OnInit {
+  characters: any;
+
+  constructor(private rickAndMortyService: RickAndMortyService) {}
+
+  ngOnInit(): void {
+    this.rickAndMortyService.getCharacters().subscribe(data => {
+      this.characters = data;
+      console.log(this.characters); // Puedes manejar los datos de la respuesta aquí
+    });
+  }
 }
